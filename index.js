e23e78e96b1be906fce4d16144a84d159b0cd06f
ThatAsian11 <sadatshams@gmail.com>
2020-01-12 18:19:28 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+// var app = require('express')();
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+var axios = require('axios');
+
+require('dotenv').config();
+
+app.use(express.static(__dirname + '/public'));
+
+
+let api = process.env.API;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/book.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  socket.on('search', function(msg) {
+    io.emit('load');
+    let url = 'https://www.googleapis.com/books/v1/volumes?q=' + msg + '&key=' + api;
+    axios.get(url)
+      .then(function (response) {
+        // handle success
+        // console.log(response.data);
+        io.emit('loaded');
+        io.emit('searched', response.data['items']);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  });
+  socket.on('content', id => {
+    let url = "https://www.googleapis.com/books/v1/volumes/" + id
+    axios.get(url)
+      .then(function(response) {
+        io.emit('book', response.data);
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
---
+++ public/scripts.js
@@ -0,0 +1,140 @@
+//var socket = io.connect('http://' + document.domain + ':' + location.port);
+var socket = io();
+
+const hide = () => {
+    document.getElementById('overlay').style.display = "none";
+};
+const data = e => {
+    console.log(e);
+    socket.emit('content', e);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('form').onsubmit = () => {
+        var query = document.querySelector('#s').value;
+        
+        socket.emit('search', query);
+        // document.querySelector('#s').value = '';
+        return false
+    };
+    document.querySelector('#clear').onclick = () => {
+        var form = document.querySelector('#s');
+        form.value = '';
+        form.focus();
+
+    };
+
+    socket.on('load', () => {
+        document.getElementById('loading').style.display = "block";
+    });
+    socket.on('loaded', () => {
+        document.getElementById('loading').style.display = "none";
+    });
+    
+    socket.on('book', results => {
+        console.log(results);
+        var img = results['volumeInfo']['imageLinks']['thumbnail'];
+        var title = results['volumeInfo']['title'];
+        var author = results['volumeInfo']['authors'];
+        var desc = results['volumeInfo']['description'];
+        var page = results['volumeInfo']['pageCount'];
+        var rating = results['volumeInfo']['averageRating'];
+        var release = results['volumeInfo']['publishedDate'];
+        document.querySelector('#book-img').src = img;
+        document.querySelector('#book-title').textContent = title;
+        document.querySelector('#book-author').textContent = author;
+        document.querySelector('#book-desc').innerHTML = desc;
+        document.querySelector('#book-page').textContent = page;
+        document.querySelector('#book-rating').textContent = rating + "/5";
+        document.querySelector('#book-release').textContent = release;
+        document.getElementById('overlay').style.display = "block";
+    });
+
+    socket.on('searched', results => {
+        // console.log(results);
+        if (!results) {
+            document.getElementById('cards').innerHTML = '';
+            var section = document.querySelector('#results');
+            var message = document.createElement('h2');
+            message.textContent = "No results found";
+            section.appendChild(message);
+        } else {
+            var nores = document.querySelector('#results h2');
+            if (nores) {
+                nores.innerHTML = '';
+            }
+            var cards = document.querySelector('#cards');
+            cards.innerHTML = "";
+            for (i in results) {
+                var pic = results[i]['volumeInfo']['imageLinks']['thumbnail'];
+                var name = results[i]['volumeInfo']['title'];
+                var details = results[i]['volumeInfo']['authors'] + ' | ' + results[i]['volumeInfo']['publishedDate'];
+                var address = results[i]['id'];
+                var text = results[i]['volumeInfo']['description'];
+                var trimmedText = '';
+                var length = 500;
+                if (text) {
+                    var textSize = text.length;
+                    var trimmedText = textSize > length ? text.substring(0, length) + "..." : text;
+                };
+    
+                var card = document.createElement('div');
+                card.className = "card mb-3 book";
+                cards.appendChild(card);
+    
+                var cont = document.createElement('div');
+                cont.className = "row no-gutters";
+                card.appendChild(cont);
+                
+                var col1 = document.createElement('div');
+                col1.className = "col-md-4";
+                cont.appendChild(col1);
+    
+                var img = document.createElement('img');
+                img.src = pic;
+                img.className = "card-img";
+                col1.appendChild(img);
+    
+                var col2 = document.createElement('div');
+                col2.className = "col-md-8";
+                cont.appendChild(col2)
+    
+                var body = document.createElement('div');
+                body.className = "card-body";
+                col2.appendChild(body);
+    
+                var title = document.createElement('h5');
+                title.className = "card-title";
+                title.textContent = name;
+                body.appendChild(title);
+    
+                var desc = document.createElement('p');
+                desc.className = "card-title";
+                desc.textContent = trimmedText;
+                body.appendChild(desc);
+    
+                var det = document.createElement('p');
+                det.className = "card-title";
+                body.appendChild(det);
+                
+                var sm = document.createElement('small');
+                sm.className = "text-muted"
+                sm.textContent = details;
+                det.appendChild(sm)
+    
+                var form = document.createElement('form');
+                form.action = " ";
+                form.id = "content"
+                body.appendChild(form);
+                // form.onsubmit = data(results[i]["id"]);
+    
+                var btn = document.createElement('button');
+                btn.className = "btn btn-outline-dark btn-sm";
+                btn.textContent = "More info";
+                btn.id = address;
+                btn.onclick = function() { data(this.id); };
+                body.appendChild(btn);
+            };
+        };
+    });
+});
